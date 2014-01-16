82be642283ce4cba6c6c91fe8e53a5b9572d01a2
jupegarnica <jupegarnica@gmail.com>
2014-01-16 21:25:11 +0000

    autoscroll mode

--- assets/js/site-ck.js
+++ assets/js/site-ck.js
@@ -1,29 +1,55 @@
-/* Dribbble Shots using Jribbble plugin */
+/* Scroll automatico para cambiar de seccion */
 $(document).ready(function() {
-    document.body.style.overflow = 'hidden';
-    
-//     $.jribbble.getShotsByPlayerId("powdah", function(e) {
-//         var t = [];
-//         $.each(e.shots, function(e, n) {
-//             t.push('<li><a href="' + n.url + '">');
-//             t.push('<img src="' + n.image_url + '" ');
-//             t.push('alt="' + n.title + '"><span class="d-hover">' + n.title + "<p>view on dribbble</p></span></a></li>")
-//         });
-//         $(".slides").html(t.join(""))
-//     }, {
-//         page: 1,
-//         per_page: 8
-//     })
+    document.body.style.overflow = 'hidden'; //scroll descativado
+    var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+    $('body').bind(mousewheelevt, function(e) {
+        var evt = window.event || e //equalize event object     
+        evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+        var delta = evt.detail ? evt.detail * (-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+        if(delta > 0) {
+            //scroll UP
+            scrollingUp();
+        } else {
+            //scroll down
+            scrollingDown();
+        }
+    });
 });
-$(function() {
-//     $(".service").hover(function() {
-//         $("#networks").addClass($(this).data("network")).addClass("active");
-//         $("#networks p").html($(this).data("tip"))
-//     }, function() {
-//         $("#networks").removeClass();
-//         $("#networks p").html("You can find me all over the web")
-//     })
-});
+var scroll = 0;
+
+function scrollingUp() {
+    scroll += 1;
+    previusSecction(scroll);
+    console.log(scroll);
+}
+
+function scrollingDown() {
+    scroll -= 1;
+    nextSecction(scroll);
+    console.log(scroll);
+}
+
+function previusSecction(n) {
+    setTimeout(function() {
+        if(n === scroll) {
+            console.log('toPreviusSection -->');
+            $("html,body").animate({
+                scrollTop: $('#chat').offset().top
+            }, 1e3)
+        }
+    }, 100);
+}
+
+function nextSecction(n) {
+    setTimeout(function() {
+        if(n === scroll) {
+            console.log('toPreviusSection -->');
+            $("html,body").animate({
+                scrollTop: $('#app').offset().top
+            }, 1e3)
+        }
+    }, 100);
+}
 jQuery(document).ready(function() {
     $("#hero").height($(window).height());
     $(window).resize(function() {
@@ -32,13 +58,18 @@ jQuery(document).ready(function() {
     });
     $("#work").height($(window).height());
     $(window).resize(function() {
-        $("#work").minHeight($(window).height());
-        $(".showcase-info").css("min-height", "360px")
+        $("#work").height($(window).height());
+        $("#work").css("max-height","360px");
+    });
+    $("#app").height($(window).height());
+    $(window).resize(function() {
+        $("#app").height($(window).height());
+        $("#app").css("max-height","360px");
     });
     $("#networks").height($(window).height());
     $(window).resize(function() {
         $("#networks").height($(window).height());
-        $("#networks").css("min-height", "360px")
+        $("#networks").css("min-height", "360px");
     })
 });
 jQuery(document).ready(function(e) {
@@ -47,6 +78,7 @@ jQuery(document).ready(function(e) {
         e("html,body").animate({
             scrollTop: e(this.hash).offset().top
         }, 1e3)
+        console.log(this.hash);
     })
 });
 /*Change, in the first line the username "powdah" with your dribbble username. That's all */
